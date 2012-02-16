7aaaa8a8a232d92c0e70c843cad78e6ba40f4aeb
Scott Cederberg <Scott.Cederberg@gmail.com>
2012-02-16 03:09:49 -0800

    Add a fancy greeting option to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,12 @@
 public class Hello {
 
     public static void main(String[] args) {
-	System.out.println("Hello, git user!  You are awesome!");
+	if (args.length > 0 && "-f".equals(args[0])) {
+	    System.out.println("~~~~ Ooooh... now that's *fancy*!~~~~");
+	    System.out.println("Hello there!");
+	} else {
+	    System.out.println("Hello, git user!  You are awesome!");
+	}
     }
 
 }
